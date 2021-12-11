03cc1d38cbb26fd8f0dd56773cec951ec46c559d
lennart <lsboecken@gmail.com>
2021-12-10 20:41:00 -0500

    refactor: extract models from app.py, currently broken

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
+from server.sql_models import Users, Messages, Channels
 import functools
 from flask import Flask, send_from_directory, request
 from flask_session import Session
-from flask_login import LoginManager, current_user, UserMixin, login_user, logout_user
+from flask_login import LoginManager, current_user, login_user, logout_user
 from flask_socketio import SocketIO, emit, disconnect
 from flask_sqlalchemy import SQLAlchemy
-
 # Flask App init
 app = Flask(__name__, static_folder="client/build", static_url_path="")
 
@@ -46,26 +46,6 @@ app.config['SESSION_SQLALCHEMY'] = db
 flask_session = Session(app)
 
 # flask SQL alchemy models
-
-
-class Users(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    username = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
-
-
-class Channels(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    name = db.Column(db.String(80), nullable=False)
-
-
-class Messages(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    message = db.Column(db.Text, nullable=False)
-    timestamp_utc = db.Column(db.Integer, nullable=False)
-    channel_id = db.Column(db.Integer, db.ForeignKey(
-        'channels.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
 
 # Routing
---
+++ server/sql_models.py
@@ -0,0 +1,22 @@
+from app import db
+from flask_login import UserMixin
+
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+
+class Channels(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+
+
+class Messages(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    timestamp_utc = db.Column(db.Integer, nullable=False)
+    channel_id = db.Column(db.Integer, db.ForeignKey(
+        'channels.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
