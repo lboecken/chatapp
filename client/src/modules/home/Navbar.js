3657f6775005377959323b9415827c98bafee07c
lennart <lsboecken@gmail.com>
2022-01-18 12:13:16 -0500

    feat: redirect on logout

--- client/src/modules/home/Navbar.js
+++ client/src/modules/home/Navbar.js
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar({ setCurrentRoom, possibleRooms, setIsLoggedIn }) {
+  const redirect = useRedirector();
   return (
     <div>
       <div css={roomsNavWrapper}>
@@ -30,6 +31,7 @@ function Navbar({ setCurrentRoom, possibleRooms, setIsLoggedIn }) {
         onClick={function Logout() {
           setIsLoggedIn(false);
           axios.post('../api/logout');
+          redirect('../login', { replace: true });
         }}>
         <FontAwesomeIcon icon={faSignOutAlt} />
       </button>
